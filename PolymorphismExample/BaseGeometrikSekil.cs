dedb946704c655f7b1a3eefc6558cb2336d4077c
abdndmn <abdndmn@hotmail.com>
2025-01-16 01:19:24 +0300

    Add project files.

---
+++ PolymorphismExample/BaseGeometrikSekil.cs
@@ -0,0 +1,46 @@
+﻿
+using System.Threading.Channels;
+
+namespace PolymorphismExample
+{
+    public class BaseGeometrikSekil//BaseClass
+    {
+        public double Genislik { get; set; }//Propertiler
+        public double Yukseklik { get; set; }
+
+        public virtual double AlanHesapla()//virtual keywordü ile override aldıgında değiştirilebilecek
+        {
+            return Genislik * Yukseklik;
+        }
+
+    }
+
+    internal class Kare : BaseGeometrikSekil//Türetilmiş class
+    {
+        public override double AlanHesapla()
+        {
+            return base.AlanHesapla();
+        }
+
+
+    }
+
+    internal class Dikdortgen : BaseGeometrikSekil//Türetilmiş class
+    {
+        public override double AlanHesapla()
+        {
+            return base.AlanHesapla();
+        }
+
+    }
+
+    internal class DikUcgen : BaseGeometrikSekil//Türetilmiş class
+    {
+
+        public override double AlanHesapla()
+        {
+            return Genislik*Yukseklik/2;//alan hesabı farkı, override keywordü ile değiştirilebildi.
+        }
+
+    }
+}
---
+++ PolymorphismExample/Program.cs
@@ -0,0 +1,23 @@
+﻿using PolymorphismExample;
+
+BaseGeometrikSekil kare = new Kare
+{
+    Genislik=10,
+    Yukseklik=10,
+};
+Console.WriteLine("Karenin Alanı: "+kare.AlanHesapla());
+
+BaseGeometrikSekil dikdortgen = new Dikdortgen
+{
+    Genislik = 10,
+    Yukseklik = 25,
+};
+
+Console.WriteLine("Dikdörtgenin Alanı: " + dikdortgen.AlanHesapla());
+
+BaseGeometrikSekil dikUcgen = new DikUcgen
+{
+    Genislik = 10,
+    Yukseklik = 30,
+};
+Console.WriteLine("Dik Üçgenin Alanı: " + dikUcgen.AlanHesapla());
